f8dfc8610c95f9f82019e7e63fe799fa0762bf30
loganjameshart <116290186+loganjameshart@users.noreply.github.com>
2022-10-20 10:10:45 -0500

    Update Time Sheet Inputter.py

--- Time Sheet Inputter.py
+++ Time Sheet Inputter.py
@@ -1,11 +1,13 @@
 import pyautogui as pag
 import time
 
-
+#sleep for a few seconds to give time to switch windows, if necessary
 time.sleep(3)
 
+#clicks in the first text box
 pag.click(272,476)
 
+#inputs my standard work hours, tabbing over to get to each new day. Pauses inbetween are used to account for the system's delay in populating drop-down choices.
 for days in range(0,5):
     pag.write("time")
     time.sleep(.5)
@@ -15,6 +17,7 @@ for days in range(0,5):
     pag.press("tab")
     pag.press("tab")
 
+#click on the text box for the next week.     
 pag.click(272,782)
 
 for days in range(0,5):
